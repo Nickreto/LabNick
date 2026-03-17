9b984276ef2e8b1cb489d128dc992dd7ab3432f4
Nick <lipin.nik.0956@gmail.com>
2026-03-17 08:43:42 +0330

    Название обозначений теперь с маленькой буквы

--- FirstIonideProject/Lab4_1003.fs
+++ FirstIonideProject/Lab4_1003.fs
@@ -9,20 +9,31 @@ let infix root left right = (left(); root(); right())
 let iterh trav f t =
         let rec tr t h =
             match t with
-            Node (x,L,R) -> trav
+            Node (x,l,r) -> trav
                                 (fun () -> (f x h)) // обход корня
-                                (fun () -> tr L (h+1)) // обход левого поддерева
-                                (fun () -> tr R (h+1)); // обход прав. поддерева
+                                (fun () -> tr l (h+1)) // обход левого поддерева
+                                (fun () -> tr r (h+1)); // обход прав. поддерева
             | Nil -> ()
         tr t 0
-let spaces n = List.fold (fun s _ -> s+"  ") "" [0..n]
-let print_tree T = iterh infix (fun x h -> printfn "%s%A" (spaces h)x) T
+let spaces n = 
+    List.fold 
+        (
+            fun s _ -> s+"  "
+        )
+        "" 
+        [0..n]
+let print_tree t = 
+    iterh infix 
+        (
+            fun x h -> printfn "%s%A" (spaces h)x
+        ) 
+        t
 let rec insert t x =
         match t with
             Nil -> Node(x,Nil,Nil)
-            | Node(z,L,R) -> 
-                if x<z then Node(z,insert L x ,R)
-                else Node(z,L,insert R x)
+            | Node(z,l,r) -> 
+                if x<z then Node(z,insert l x ,r)
+                else Node(z,l,insert r x)
     //Функция свертки дерева
 let tree_fold f st tree =   
     let rec ob d k = 
@@ -36,7 +47,7 @@ let rec map f tree =
     | Nil -> Nil
     | Node(v, left, right) -> Node(f v, map f left, map f right)
 
-let FirstSymbol (symb: char) (word: string)= 
+let firstSymbol (symb: char) (word: string)= 
     string symb + word.[1..]
 
 let rec createWord n str =
@@ -85,25 +96,25 @@ let task1 _ =
         printfn "Неверный ввод"
         0
     else
-    let A =
+    let a =
         [
             let str = ""
             let r = new Random()
             for i in 1..n do
                 yield createWord (r.Next( 2,10)) str
         ]
-    printfn "Исходный список %A" A
-    let BT = A |> List.fold insert Nil
-    print_tree BT
+    printfn "Исходный список %A" a
+    let bt = a |> List.fold insert Nil
+    print_tree bt
     printfn "Введите символ для замены: "
-    let NewSymb = CharInput 1
-    if NewSymb = char (0) then 
+    let newSymb = CharInput 1
+    if newSymb = char (0) then 
         0
     else
-    let NewBT = BT |> map (FirstSymbol NewSymb)
-    printfn "Вывод изменённого дерева: "
-    print_tree NewBT    
-    0
+        let newBT = bt |> map (firstSymbol newSymb)
+        printfn "Вывод изменённого дерева: "
+        print_tree newBT    
+        0
 
 let task2 _ =
     printfn "Введите количество элементов: "
@@ -112,18 +123,18 @@ let task2 _ =
         printfn "Неверный ввод"
         0
     else
-    let A =
+    let a =
         [
             let str = ""
             let r = new Random()
             for i in 1..n do
                 yield createWord (r.Next( 2,10)) str
         ]
-    printfn "Исходный список %A" A
-    let BT = A |> List.fold insert Nil
-    print_tree BT
-    let NewBT = leafFold [] BT |> List.rev
-    printfn "Вывод списка листьев: %A" NewBT  
+    printfn "Исходный список %A" a
+    let bt = a |> List.fold insert Nil
+    print_tree bt
+    let newBT = leafFold [] bt |> List.rev
+    printfn "Вывод списка листьев: %A" newBT  
     0
 
 let body _ =
